d58df10205a054f3233c20a706715a98734481c6
Nicolas Kao Sartor <nicolas@unum.la>
2019-10-08 21:58:18 -0700

    connecting to postgres heroku

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const createError = require("http-errors");
 
+const baseDir = process.cwd();
+const PostgreSQL = require(baseDir + "/services/PostgreSQL/index.js");
+
+require("dotenv").config();
+
 const app = express();
 const PORT = process.env.PORT || "3000";
 
+PostgreSQL.connect();
+
 app.set("port", PORT);
-app.listen(PORT, () => console.log(`[Server listening on port ${PORT}]`));
+app.listen(PORT, () => {
+	console.log(`[Server listening on port ${PORT}]`);
+});
 
 //Route Loader
 require("./routes")(app);
--- services/PostgreSQL/index.js
+++ services/PostgreSQL/index.js
@@ -1,2 +1,22 @@
 const { Client } = require("pg");
-const client = new Client();
+
+const client = new Client({
+	connectionString: "postgres://vlxzcpmlnqtqpi:a9bde716908b9e7232bf6685d70aafffafa672a51ef908f9c1515c4aaf7d3717@ec2-174-129-227-146.compute-1.amazonaws.com:5432/d14941h8504b24",
+	ssl: true
+});
+
+const connect = async () => {
+	client
+		.connect()
+		.then(() => {
+			console.log("[POSTGRESQL CONNECTED SUCCESSFULLY]");
+		})
+		.catch(err => {
+			console.log("[POSTGRESQL CONNECTION FAILED]", err);
+		})
+		.finally(() => client.end());
+};
+
+module.exports = {
+	connect
+};
