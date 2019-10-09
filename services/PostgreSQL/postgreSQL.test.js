a8f9114095fbadede31f79561d8e88f32903e880
Nicolas Kao Sartor <nicolas@unum.la>
2019-10-08 17:44:24 -0700

    added initial database test structure

---
+++ services/PostgreSQL/postgreSQL.test.js
@@ -0,0 +1,23 @@
+const postgreSQL = require("./index");
+
+test("Connects to database", async () => {
+	// will connect
+});
+
+/**
+ * CRUD Tests
+ */
+describe("Tests CRUD operations", () => {
+	test("Writes to database", async () => {
+		// will write
+	});
+	test("Reads from database", async () => {
+		// will read
+	});
+	test("Updates to database", async () => {
+		// will update
+	});
+	test("Deletes from database", async () => {
+		// will delete
+	});
+});
